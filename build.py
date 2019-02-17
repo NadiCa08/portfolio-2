2c63c9df2fb399d5b245a13d79c50d1294585c6a
nadica08 <nadiabc3@gmail.com>
2019-02-16 17:07:38 -0800

    split files and combined into oned py file

--- build.py
+++ build.py
@@ -1,1 +1,21 @@
-portfolio_site_2.4
+print('site push')
+top = open('templates/top.html').read()
+
+body = open('body/activities.html').read()
+
+body2 = open('body/funfacts.html').read()
+
+bottom = open('templates/bottom.html').read()
+
+
+combined_html = top + body + bottom 
+open('docs/built_file.html', 'w+').write(combined_html)
+
+
+combined_html_activities = top + body2 + bottom
+open('docs/built_file_activities.html', 'w+').write(combined_html_activities)
+
+
+
+
+
